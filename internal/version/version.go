83128e07b7d1938438743d60d88597036ce92299
code <chrobione@gmail.com>
2026-02-23 22:28:40 -0600

    Fix orphan manager template rendering and adopt action bindings

--- internal/version/version.go
+++ internal/version/version.go
@@ -23,7 +23,7 @@ import (
 // This is set at build time via ldflags:
 //
 //	-X github.com/friendsincode/grimnir_radio/internal/version.Version=X.Y.Z
-var Version = "1.17.30"
+var Version = "1.17.31"
 
 // GitHubRepo is the repository to check for updates
 const GitHubRepo = "friendsincode/grimnir_radio"
